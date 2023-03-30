54921fa967db997b596b81f749ebf54d19257f28
Gagu <gagu@insowe.com>
2023-03-30 20:51:09 +0800

    功能：Bing

--- base/message.js
+++ base/message.js
@@ -1,8 +1,13 @@
 import gpt from "../libs/gpt.js";
+import bing from "../libs/bing.js";
 
 const getReply = async (message, key) => {
   if (message.toLowerCase().startsWith("gpt ")) {
     const resp = await gpt.getGptResponse(message.substring(4), key);
+    return resp.trim();
+  }
+  if (message.toLowerCase().startsWith("bing ")) {
+    const resp = await bing.getResponse(message.substring(5));
     return resp.trim();
   }
 
---
+++ libs/bing.js
@@ -0,0 +1,31 @@
+import fs from "fs";
+import path from "path";
+import { BingChat } from "bing-chat";
+
+const __dirname = path.resolve();
+
+const config = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "config", "bing.json"), "utf8")
+);
+
+const api = new BingChat({
+  cookie: config.cookie,
+});
+
+const getResponse = async (message) => {
+  try {
+    const res = await api.sendMessage(message);
+    return res.text;
+  } catch (error) {
+    return "錯誤(bing)";
+  }
+};
+
+// (async () => {
+//   const res = await getResponse("你好");
+//   console.log(res);
+// })();
+
+export default {
+  getResponse,
+};
