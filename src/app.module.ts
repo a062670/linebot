4b66749e50ee98047a91e2725b6b18e7cdd2fe7b
gagu-home <gagu@insowe.com>
2024-04-30 00:08:04 +0800

    修改：Line：讀不到 ACCESS_TOKEN

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+// 必須寫在上面
+const envModule = ConfigModule.forRoot({
+  isGlobal: true,
+});
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LineModule } from './modules/line/line.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    LineModule,
-  ],
+  imports: [envModule, LineModule],
   controllers: [AppController],
   providers: [AppService],
 })
