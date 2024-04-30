e6539c99348149eee15f6b53d6b9217b16e8f6e4
gagu-home <gagu@insowe.com>
2024-04-30 21:38:22 +0800

    修改：rebuild-docker.sh 不先 rmi

--- sh/rebuild-docker.sh
+++ sh/rebuild-docker.sh
@@ -5,7 +5,7 @@ sudo git merge '@{u}'
 
 sudo docker stop linebot
 sudo docker rm linebot
-sudo docker rmi -f linebot
+# sudo docker rmi -f linebot
 
 sudo docker build --build-arg GIT_COMMIT=$(git rev-parse HEAD) -t linebot .
 
