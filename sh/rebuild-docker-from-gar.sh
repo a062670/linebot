7a3ad0767c072840c969cc40d1cc425aed7a324e
gagu-home <gagu@insowe.com>
2024-09-29 22:20:49 +0800

    修改：修改部屬方式

--- sh/rebuild-docker-from-gar.sh
+++ sh/rebuild-docker-from-gar.sh
@@ -1,13 +1,10 @@
-cd /var/projects/linebot/
-sudo git fetch
-sudo git reset --hard HEAD
-sudo git merge '@{u}'
+source /sh/.env
 
 sudo docker stop linebot
 sudo docker rm linebot
 sudo docker rmi -f linebot
 
-sudo docker build --build-arg GIT_COMMIT=$(git rev-parse HEAD) -t linebot .
+sudo docker pull $GAR/linebot:latest
 
 cd /var/projects/linebot/docker/
 sudo docker-compose up -d
